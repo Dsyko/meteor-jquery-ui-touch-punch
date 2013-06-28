50c79fb0c3a97fffad1a15f9fdf384fe2276a43c
David Sykora <dsykora@gmail.com>
2013-06-28 15:35:51 -0700

    Pulling in the package

---
+++ package.js
@@ -0,0 +1,8 @@
+Package.describe({
+	summary: "jquery-ui-touch-punch project, Enable iPad/Phone/Pod touch interaction to jQuery UI controls"
+});
+
+Package.on_use(function (api){
+	api.add_files('touch-punch/jquery.ui.touch-punch.min.js', 'client');
+
+});
